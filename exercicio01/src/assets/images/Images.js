9561090213e5ad9c99bb2611988a02b37798e1f4
Kaique Parente <kaique.parente.game@outlook.com>
2024-08-22 13:27:52 -0300

    Component Card and Assets

---
+++ exercicio01/src/assets/images/Images.js
@@ -0,0 +1,48 @@
+import alcatraImagem from'./products/alcatra.jpg';
+import baiaoImagem from './products/baiao.jpg';
+import batataFritaImagem from './products/batata-frita.jpg';
+import caipirinhaImagem from './products/caipirinha.jpg';
+import carneSolImagem from './products/carne-sol.jpg';
+import choppImagem from './products/chopp.jpg';
+import churrascoCoracaoImagem from './products/churrasco-coracao.jpg';
+import churrascoFrangoImagem from './products/churrasco-frango.jpg';
+import costelaImagem from './products/costela.jpg';
+import deliciaMorangoImagem from './products/delicia-morango.jpg';
+import escondidinhoImagem from './products/escondidinho.jpg';
+import fraldinhaImagem from './products/fraldinha.jpg';
+import linguicaImagem from './products/linguica.jpg';
+import mousseMaracujaImagem from './products/mousse-maracuja.jpg';
+import paoAlhoImagem from './products/pao-alho.jpg';
+import picanhaImagem from './products/picanha-espeto.jpg';
+import pudimImagem from './products/pudim.jpg';
+import refrigeranteImagem from './products/refrigerante.jpg';
+import sucoAbacaxiImagem from './products/suco-abacaxi.jpg';
+import sucoLaranjaImagem from './products/suco-laranja.jpg';
+import vinhoImagem from'./products/vinho.jpg';
+
+const images = {
+    picanha: picanhaImagem,
+    fraldinha: fraldinhaImagem,
+    alcatra: alcatraImagem,
+    baiao: baiaoImagem,
+    batataFrita: batataFritaImagem,
+    caipirinha: caipirinhaImagem,
+    carneSol: carneSolImagem,
+    chopp: choppImagem,
+    churrascoCoracao: churrascoCoracaoImagem,
+    churrascoFrango: churrascoFrangoImagem,
+    costela: costelaImagem,
+    deliciaMorango: deliciaMorangoImagem,
+    escondidinho: escondidinhoImagem,
+    linguica: linguicaImagem,
+    mousseMaracuja: mousseMaracujaImagem,
+    paoAlho: paoAlhoImagem,
+    pudim: pudimImagem,
+    refrigerante: refrigeranteImagem,
+    sucoAbacaxi: sucoAbacaxiImagem,
+    sucoLaranja: sucoLaranjaImagem,
+    vinho: vinhoImagem
+  };
+
+export default images;
+
---
+++ exercicio01/src/components/Card.js
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+import images from "../assets/images/Images";
+import Title from "./Title";
+import Description from "./Description";
+
+const CardContainer = styled.div`
+    width:400px;
+    height: 600px;
+    
+    background-color: #ffffff;
+    text-align: center;
+
+    border-radius: 8px;
+    box-shadow: rgba(188,188,188,255) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
+ 
+
+    @media (max-width: 1052px) {
+        width: 350px;
+    }
+
+    @media (max-width: 949px) {
+        width: 300px;
+    }
+
+    @media (max-width: 690px) {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        padding-bottom: 80px;
+        width: 250px;
+    }
+
+    @media (max-width: 560px) {
+        width: 400px;
+        padding-bottom: 120px;
+    }
+`
+
+const CardText = styled.div`
+    padding: 40px;
+
+    @media (max-width: 1052px) {
+        padding: 0px 20px;
+    }
+`
+
+const Image = styled.img`
+    width: 100%;
+    height: 250px;
+    border-radius: 8px 8px 0px 0px;
+
+    margin-bottom: 40px;
+
+    @media (max-width: 690px) {
+        margin-bottom: 40px;
+    }
+`
+
+export default function Card({image, title, description}){
+    return(
+        <CardContainer>
+            <Image src={images[image]} alt={title} />
+            <CardText>
+                <div>
+                    <Title>{title}</Title>
+                    <Description>{description}</Description>
+                </div>
+            </CardText>
+            
+        </CardContainer>
+    );
+}
